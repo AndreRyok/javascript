1a224b5cbeb0da0e7137c9a16124358278953934
AndreRyok <62232432+AndreRyok@users.noreply.github.com>
2024-08-22 23:26:21 -0300

    att

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,1 +1,6 @@
-console.log('ESTOU TESTANDO ESSA PORRAAA!!!!!!');8
+var vel = 85;
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60){ //Condição simples!
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
