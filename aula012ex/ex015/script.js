078a3be5dcb03d643f9a0e35293915652b3d112c
AndreRyok <62232432+AndreRyok@users.noreply.github.com>
2024-09-03 15:23:57 -0300

    att

--- aula012ex/ex015/script.js
+++ aula012ex/ex015/script.js
@@ -2,7 +2,6 @@ function verificar() {
     var data = new Date();
     var ano = data.getFullYear();
     var fano = document.getElementById('txtano');
-    var img = document.getElementById('imagem')
     var res = document.getElementById('res');
     if (fano.value.length == 0 || Number(fano.value) >= ano) {
         alert('Verifique os dados e tente novamente!');
@@ -10,41 +9,45 @@ function verificar() {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var gênero = ''
+        var img = document.createElement('img')
+
+        img.setAttribute('id', 'foto')
         
 
         if (fsex[0].checked) {
-            gênero = 'Homem'
+            gênero = 'homem'
             if (idade > 0 && idade <= 11){
-                img.src = 'bebe-m.png'
+                img.setAttribute('src', 'bebe-m.png') 
                 document.body.style.backgroundColor = 'rgb(88, 137, 236)'
             } else if (idade < 21) {
-                img.src = 'jovem-m.png'
+                img.setAttribute('src', 'jovem-m.png') 
                 document.body.style.backgroundColor = 'rgb(88, 137, 236)'
             } else if (idade < 65) {
-                img.src = 'adulto-m.png'
+                img.setAttribute('src','adulto-m.png' ) 
                 document.body.style.backgroundColor = 'rgb(88, 137, 236)'
             } else {
-                img.src = 'idoso-m.png'
+                img.setAttribute('src', 'idoso-m.png') 
                 document.body.style.backgroundColor = 'rgb(88, 137, 236)'
             }
         } else if (fsex[1].checked) {
-            gênero = 'Mulher'
+            gênero = 'mulher'
             if (idade > 0 && idade <= 11){
-                img.src = 'bebe-f.png'
+                img.setAttribute('src', 'bebe-f.png') 
                 document.body.style.backgroundColor = 'rgb(243, 139, 188)'
             } else if (idade < 21) {
-                img.src = 'jovem-f.png'
+                img.setAttribute('src', 'jovem-f.png') 
                 document.body.style.backgroundColor = 'rgb(243, 139, 188)'
             } else if (idade < 65) {
-                img.src = 'adulta-f.png'
+                img.setAttribute('src','adulta-f.png' ) 
                 document.body.style.backgroundColor = 'rgb(243, 139, 188)'
             } else {
-                img.src = 'idosa-f.png'
+                img.setAttribute('src', 'idosa-f.png') 
                 document.body.style.backgroundColor = 'rgb(243, 139, 188)'
             }
         }
         res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.innerHTML = `Detectamos: ${gênero} com ${idade} anos.`
+        res.appendChild(img)
     }
 
 }
